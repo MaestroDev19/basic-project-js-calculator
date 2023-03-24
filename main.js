3d3c0b2600fdbc90aada33bbd4adf0a82a7ce51a
Charles <charles19rex@gmail.com>
2023-03-24 21:57:16 +0100

    funtionality completed

--- main.js
+++ main.js
@@ -1,59 +1,56 @@
 let textArea = document.querySelector('#textArea');
 
-
-function num1(){
-    textArea.value += "1";
-}
-
-function num2(){
-    textArea.value +="2"
-}
-function num3(){
-    textArea.value +="3"
-}
-
-function num4(){
-    textArea.value +="4"
-}
-
-function num5(){
-    textArea.value +="5"
-}
-
-function num6(){
-    textArea.value +="6"
-}
-
-function num7(){
-    textArea.value +="7"
-}
-
-function num8(){
-    textArea.value +="8"
-}
-
-function num9(){
-    textArea.value +="9"
-}
-
-function num() {
-    textArea.value +="0"
-}
+ function digits(digit){
+    textArea.value += digit;
+ }
 
 function add(){
-    textArea.value += "+"
+    textArea.value += "+";
 }
 
 function sub(){
-    textArea.value += "-"
+    textArea.value += "-";
+}
+
+function mul(){
+    textArea.value += "*";
+}
+
+function div(){
+    textArea.value += "÷";
+}
+
+function exp(){
+    textArea.value += "**";
+}
+
+function mod(){
+    textArea.value += "%";
+}
+
+function b_left(){
+    textArea.value += "(";
+}
+
+function b_right(){
+    textArea.value += ")";
+}
+
+function dec(){
+    textArea.value +=".";
+}
+
+function root(){
+    textArea.value += Math.sqrt(textArea.value) ;
 }
 
 function solve(){
     answer = eval(textArea.value);
     textArea.value = answer;
 }
-
-function clear(){
-    textArea.value ="";
-    textArea.textContent = "";
+ function del(){
+    textArea.value = textArea.value.slice(0,-1);
+ }
+function cleanUp() {
+    textArea.value = "";
 }
