d998a8c993c6294c04b3fc21330ac927fda610bd
Charles <charles19rex@gmail.com>
2023-03-25 22:03:27 +0100

    Upadated ui and optimized js

--- main.js
+++ main.js
@@ -1,56 +1,24 @@
-let textArea = document.querySelector('#textArea');
+let textArea = document.querySelector("#textArea");
 
- function digits(digit){
-    textArea.value += digit;
- }
-
-function add(){
-    textArea.value += "+";
+function digits(digit) {
+  textArea.value += digit;
 }
 
-function sub(){
-    textArea.value += "-";
+function opps(sign) {
+  textArea.value += sign;
 }
 
-function mul(){
-    textArea.value += "*";
+function root() {
+  textArea.value += Math.sqrt(textArea.value);
 }
 
-function div(){
-    textArea.value += "÷";
+function solve() {
+  answer = eval(textArea.value);
+  textArea.value = answer;
 }
-
-function exp(){
-    textArea.value += "**";
+function del() {
+  textArea.value = textArea.value.slice(0, -1);
 }
-
-function mod(){
-    textArea.value += "%";
-}
-
-function b_left(){
-    textArea.value += "(";
-}
-
-function b_right(){
-    textArea.value += ")";
-}
-
-function dec(){
-    textArea.value +=".";
-}
-
-function root(){
-    textArea.value += Math.sqrt(textArea.value) ;
-}
-
-function solve(){
-    answer = eval(textArea.value);
-    textArea.value = answer;
-}
- function del(){
-    textArea.value = textArea.value.slice(0,-1);
- }
 function cleanUp() {
-    textArea.value = "";
+  textArea.value = "";
 }
